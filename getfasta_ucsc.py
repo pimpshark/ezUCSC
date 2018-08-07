3c0ad07d2ab85bea4c72b7bf6b9ef857b6e27c9f
pimpshark <ravimandla97@gmail.com>
2018-08-07 12:22:48 -0700

    fix bad url issues

--- getfasta_ucsc.py
+++ getfasta_ucsc.py
@@ -21,7 +21,61 @@ def getFasta(seq, output, lib="default"):
         current = pls.read()
     list = current.split('\n')
     sequencelist = list[:len(list)-1]
-    if 'cavPor' in lib:
+    for i in sequencelist:
+        print(i)
+        chrom = i.split(':')
+        chr = chrom[0]
+        positions = chrom[1].split('-')
+        pos1int = positions[0]
+        pos1 = str(int(pos1int)-1)
+        pos2 = positions[1]
+
+        try:
+            sitept1 = 'https://genome.ucsc.edu/cgi-bin/hgc?hgsid=685472393_vBUENpT6tcuafSI3JgJpSjkORRhJ&g=htcGetDna2&table=&i=mixed&o=' + pos1 + '&l=' + pos1 + '&r=' + pos2 + '&getDnaPos=' + chr + '%3A'
+            sitept2 = pos1[:2] + '%2C' + pos1[2:5] + '%2C' + pos1[5:8] + '-' + pos2[:2] + '%2C' + pos2[2:5] + '%2C' + pos2[5:8] + '&db=' + lib
+            sitept3 = '&hgSeq.cdsExon=1&hgSeq.padding5=0&hgSeq.padding3=0&hgSeq.casing=upper&boolshad.hgSeq.maskRepeats=0&hgSeq.repMasking=lower&boolshad.hgSeq.revComp=0&submit=get+DNA'
+            site = sitept1 + sitept2 + sitept3
+
+            soup = BeautifulSoup(urllib.request.urlopen(site), "lxml")
+            for i in soup.findAll(text=True):
+                if chr in i:
+                    fasta = i[1:]
+                    break
+                elif "scaffold" in i:
+                    fasta = i
+            type(fasta)
+            print("Fasta Found!")
+            print('reading from ' + site)
+        except:
+            try:
+                sitept1 = 'https://genome.ucsc.edu/cgi-bin/hgc?hgsid=685497575_vSIBY6HIaxZ0VKQ2wZsZHchqaIz5&g=htcGetDna2&table=&i=mixed&o=' + pos1 + '&l=' + pos1 + '&r=' + pos2 + '&getDnaPos=' + chr + '%3A'
+                sitept2 = pos1[:3] + '%2C' + pos1[3:6] + '-' + pos2[:3] + '%2C' + pos2[3:6] + '&db=' + lib
+                sitept3 = '&hgSeq.cdsExon=1&hgSeq.padding5=0&hgSeq.padding3=0&hgSeq.casing=upper&boolshad.hgSeq.maskRepeats=0&hgSeq.repMasking=lower&boolshad.hgSeq.revComp=0&submit=get+DNA'
+
+                site = sitept1 + sitept2 + sitept3
+
+                soup = BeautifulSoup(urllib.request.urlopen(site), "lxml")
+                for i in soup.findAll(text=True):
+                    #print(i)
+                    if chr in i:
+                        fasta = i[1:]
+                        break
+                    elif "scaffold" in i:
+                        fasta = i
+                type(fasta)
+                print("Fasta Found!")
+                print('reading from ' + site)
+            except:
+                print("\n\nERROR in pulling Fasta\n\n")
+                print(site)
+                print("\n\n")
+                return
+    with open(output, "a") as file:
+        file.write(fasta)
+        file.write('\n')
+
+"""
+    if 'cavPor' or 'felCat' in lib:
         for i in sequencelist:
             print(i)
             chrom = i.split(':')
@@ -67,7 +121,7 @@ def getFasta(seq, output, lib="default"):
             pos1 = str(int(pos1int) - 1)
             pos2 = positions[1]
             print(pos1, pos2)
-
+            print(lib)
             sitept1 = 'https://genome.ucsc.edu/cgi-bin/hgc?hgsid=685472393_vBUENpT6tcuafSI3JgJpSjkORRhJ&g=htcGetDna2&table=&i=mixed&o=' + pos1 + '&l=' + pos1 + '&r=' + pos2 + '&getDnaPos=' + chr + '%3A'
             sitept2 = pos1[:2] + '%2C' + pos1[2:5] + '%2C' + pos1[5:8] + '-' + pos2[:2] + '%2C' + pos2[2:5] + '%2C' + pos2[5:8] + '&db=' + lib
             sitept3 = '&hgSeq.cdsExon=1&hgSeq.padding5=0&hgSeq.padding3=0&hgSeq.casing=upper&boolshad.hgSeq.maskRepeats=0&hgSeq.repMasking=lower&boolshad.hgSeq.revComp=0&submit=get+DNA'
@@ -90,7 +144,7 @@ def getFasta(seq, output, lib="default"):
             with open(output, "a") as file:
                 file.write(fasta)
                 file.write('\n')
-
+"""
 
 
 #if len(sys.argv) == 3:
